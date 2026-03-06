99ad1344befa8337979e45d0ce9caf4fa149e94c
karim-ullah <freelancerkarimullah@gmail.com>
2026-03-07 01:22:13 +0600

    fetch data from api and display all issues in the all tab or whole website load

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+const loadAllIssues = ()=>{
+    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+    fetch(url)
+    .then(res => res.json())
+    .then(issues => displayAllIssues(issues.data))
+}
+
+const displayAllIssues = (issues) =>{
+    const cardContainer = document.getElementById('card-container')
+    cardContainer.innerHTML ="";
+    issues.forEach(issueItem => {
+        
+        // console.log(issue.title);
+        
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML = `
+        
+        <div class="shadow-md rounded-2xl border-t-5 border-[${issueItem.status === 'open'? '#00A96E' : '#A855F7'}] hover:cursor-pointer">
+
+                <div class="cardHeader flex justify-between items-center px-4 pt-4">
+                    <img src="${issueItem.status === 'open' ? './assets/Open-Status.png' : './assets/closestatus.png'}" alt="status image" />
+                    <button class="bg-[#FEECEC] text-[#EF4444] px-8 py-1 rounded-full">${issueItem.priority.toUpperCase()}</button>
+                </div>
+
+                <div class="cardBody p-4 space-y-3">
+                    <h2 class="font-semibold text-lg line-clamp-1">${issueItem.title}</h2>
+                    <p class="text-sm text-[#64748B] line-clamp-2">${issueItem.description}</p>
+                    <div class="flex gap-1 items-center">
+                        <button class="bg-[#FEECEC] text-[#EF4444] px-8 py-1 rounded-full">Bug</button>
+                        <button class="bg-[#FFF8DB] text-[#D97706] px-8 py-1 rounded-full">Help wanted</button>
+                    </div>
+                    
+                </div>
+                <hr class="border-[#E4E4E7]">
+                <div class="cardFooter p-4">
+                    <p class="text-sm text-[#64748B]">#1by ${issueItem.author}</p>
+                    <p class="text-sm text-[#64748B]">${new Date(issueItem.createdAt).toLocaleDateString()}</p>
+                </div>
+            </div>
+        
+        
+        `
+        cardContainer.appendChild(newDiv)
+        
+        
+    })
+    
+}
+
+loadAllIssues()
